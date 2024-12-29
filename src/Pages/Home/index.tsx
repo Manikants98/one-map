f6ac913f916acc7753bc0d9b210f4930fa86e7a6
Mani Kant Sharma <manikants157@gmail.com>
2024-12-29 14:34:07 +0530

    mkx

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import Map from 'Components/Map'
-import Categories from 'Components/Categories'
 import { Button } from '@mui/material'
+import Categories from 'Components/Categories'
+import Map from 'Components/Map'
+import React from 'react'
+import { GridIcon } from 'Resources/SVG'
 
 const Home: React.FC = () => {
   return (
@@ -9,21 +10,20 @@ const Home: React.FC = () => {
       <Categories />
       <Map />
       <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 gap-3 lg:gap-4 flex justify-center items-center z-[999] w-full px-4 sm:w-auto">
-        <Button className="!p-0 !min-w-10 !lg:min-w-[42px]">
-          <img
-            src="https://officialguidemap.com:5173/src/assets/icons/grid.png"
-            alt=""
-            className="p-2 bg-white !border !border-black !border-opacity-50 rounded size-10 lg:size-[42px]"
-          />
+        <Button
+          color="info"
+          size="large"
+          className="!border !p-2 !bg-white !border-black !border-solid !min-w-min"
+        >
+          <GridIcon className="!text-white" />
         </Button>
-
         <Button
           disableElevation
           size="large"
           variant="contained"
-          className="!w-full sm:!w-80 !capitalize !bg-[#034885] !text-sm sm:!text-base mt-2 sm:mt-0"
+          className="!w-full lg:!mt-0.5 !p-2 sm:!w-80 !capitalize !bg-[#034885]"
         >
-          Featured Attraction
+          View Featured Attraction
         </Button>
       </div>
     </div>
